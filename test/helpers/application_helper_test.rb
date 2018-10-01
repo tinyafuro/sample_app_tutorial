6e3eb09101fa63da5bca947b10e638906b2a6fc5
tinyafuro <tinyforest.win@gmail.com>
2018-10-01 16:54:09 +0900

    Finish layout and routes

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  test "full title helper" do
+    # assert_equal full_title,         'Home'
+    # assert_equal full_title("Help"), 'Help'
+    # assert_equal full_title("About"), 'About'
+    # assert_equal full_title("Contact"), 'Contact'
+  end
+end
