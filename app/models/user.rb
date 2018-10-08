47485a4f9e54c4663c9b05eae9988c3ce149476b
tinyafuro <tinyforest.win@gmail.com>
2018-10-08 11:01:40 +0900

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
     #記憶トークンとユーザーを関連付けたトークンに対応する記憶ダイジェスト、アクティベート用トークン
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
 
     #保存する直前にメールアドレスを小文字に変換
     before_save   :downcase_email
@@ -65,6 +65,24 @@ class User < ApplicationRecord
     # 有効化用のメールを送信する
     def send_activation_email
         UserMailer.account_activation(self).deliver_now
+    end
+
+    # パスワード再設定の属性を設定する
+    def create_reset_digest
+        self.reset_token = User.new_token
+        # update_attribute(:reset_digest,  User.digest(reset_token))
+        # update_attribute(:reset_sent_at, Time.zone.now)
+        update_columns(reset_digest:  User.digest(reset_token), reset_sent_at: Time.zone.now)
+    end
+
+    # パスワード再設定のメールを送信する
+    def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now
+    end
+
+    # パスワード再設定の期限が切れている場合はtrueを返す
+    def password_reset_expired?
+        reset_sent_at < 2.hours.ago
     end
 
     private
